bf8a7d5dfbd7eba7e1ca75020656618791b993e7
Kaszydo <kaszydo@gmail.com>
2023-03-08 18:59:18 +0100

    function for creating ID

--- model/util.py
+++ model/util.py
@@ -7,4 +7,18 @@ def generate_id(number_of_small_letters=4,
                 number_of_digits=2,
                 number_of_special_chars=2,
                 allowed_special_chars=r"_+-!"):
-    return 'T!uq6-b4Yq'
+
+    random_lowercase = [random.choice(string.ascii_lowercase)
+                        for _ in range(number_of_small_letters)]
+    random_uppercase = [random.choice(string.ascii_uppercase)
+                        for _ in range(number_of_capital_letters)]
+    random_digits = [str(random.randint(0, 9))
+                     for _ in range(number_of_digits)]
+    random_chars = [random.choice(allowed_special_chars)
+                    for _ in range(number_of_special_chars)]
+    id = random_lowercase + random_uppercase + random_digits + random_chars
+    random.shuffle(id)
+    return ''.join(id)
+
+
+print(generate_id())
